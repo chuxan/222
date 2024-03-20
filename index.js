56025e4bf7f967c4c0dcee15907d3e64a67fc19b
chuxan <156198537+chuxan@users.noreply.github.com>
2024-03-20 10:13:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = "Матвей"
+function logName () {
+    console.log(`Hello, ${name}`)
+}
+    function isEven(number){
+        if (number%2 === 0) {return true} else return false
+    }
+    let person = {
+        name: "Matvey"
+        age: "17"
+        city: "Velikiy Novgorod"
+    }
+function logPerson() {
+    console.log (`Меня зовут ${person.name}. Мне ${person,age} лет. Я живу в ${person.city}.`)
+}
+let arrayNumbers = ["1","2","3","4","5"]
